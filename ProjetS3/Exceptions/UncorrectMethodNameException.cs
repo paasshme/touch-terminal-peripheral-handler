b6e53500c75c73839da63e836979b3abb568eb0c
bourdonq <xXImprimanteXx@gmail.com>
2020-01-06 17:39:55 +0100

    Created some exceptions

---
+++ ProjetS3/Exceptions/UncorrectMethodNameException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetS3
+{
+    public class UncorrectMethodNameException : Exception
+    {
+        public UncorrectMethodNameException()
+        {
+            Console.WriteLine("This method doesnt exist");
+        }
+    }
+}
