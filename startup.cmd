f790612e5278424a3d6a15b6362e143968f7c696
mironnej <Jacques.Mironneau@etu.univ-grenoble-alpes.fr>
2020-03-24 18:07:05 +0100

    Launch script windows

--- startup.cmd
+++ startup.cmd
@@ -1,3 +1,4 @@
+set /p var=<path.txt
 PowerShell -Command "Set-ExecutionPolicy Unrestricted" >> "%TEMP%\StartupLog.txt" 2>&1
-PowerShell C:\Users\Jacques\source\repos\PashmiDev\ProjetS4\winLaunch.ps1 >> "%TEMP%\StartupLog.txt" 2>&1
+PowerShell %var%\winLaunch.ps1 >> "%TEMP%\StartupLog.txt" 2>&1
  
