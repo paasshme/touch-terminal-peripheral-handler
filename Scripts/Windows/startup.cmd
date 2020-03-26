baa48f2dc5b6a7b00af75622133c322c65c183c9
mironnej <Jacques.Mironneau@etu.univ-grenoble-alpes.fr>
2020-03-26 15:28:38 +0100

    Handle space in path

--- Scripts/Windows/startup.cmd
+++ Scripts/Windows/startup.cmd
@@ -1,3 +1,3 @@
 set /p var=<"%~dp0path.txt"
-start "%var%winLaunch.cmd" >> "%TEMP%\StartupLog.txt" 2>&1
+"%var%winLaunch.cmd" >> "%TEMP%\StartupLog.txt" 2>&1
 cmd /k
--- Scripts/Windows/winLaunch.cmd
+++ Scripts/Windows/winLaunch.cmd
@@ -1,12 +1,12 @@
 @echo off
 set projectPath="..\.."
-dotnet restore %projectPath%\ProjetS3.sln
-dotnet build %projectPath%\ProjetS3.sln
-copy /Y %projectPath%\TestDevices\bin\Debug\netcoreapp3.1\TestDevices.dll %projectPath%\ProjetS3\PeripheralLibraries
+dotnet restore "%projectPath%\ProjetS3.sln"
+dotnet build "%projectPath%\ProjetS3.sln"
+copy /Y "%projectPath%\TestDevices\bin\Debug\netcoreapp3.1\TestDevices.dll" "%projectPath%\ProjetS3\PeripheralLibraries"
 start chrome --kiosk https://localhost:5001/swagger --noerrdialogs --disable-infobars --no-default-browser-check --no-experiments --no-pings --silent-debugger-extension-api
 
 ::With the terminal up
 ::start %~dp0\%projectPath%\ProjetS3\bin\Debug\netcoreapp3.1\ProjetS3.exe
 
 :: Minimize the terminal (Deployment purpose)
-start /min %~dp0\%projectPath%\ProjetS3\bin\Debug\netcoreapp3.1\ProjetS3.exe
+start /min "%~dp0\%projectPath%\ProjetS3\bin\Debug\netcoreapp3.1\ProjetS3.exe"
