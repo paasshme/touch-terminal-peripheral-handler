942023fe9ee1910ceecf1318b2f903317fc45921
mironnej <Jacques.Mironneau@etu.univ-grenoble-alpes.fr>
2020-03-26 14:19:15 +0100

    Script for windows done

--- Scripts/Windows/startup.cmd
+++ Scripts/Windows/startup.cmd
@@ -1,3 +1,4 @@
+echo "SALUT MEC!"
 set /p var=<path.txt
 PowerShell -Command "Set-ExecutionPolicy Unrestricted" >> "%TEMP%\StartupLog.txt" 2>&1
 PowerShell %var%\winLaunch.ps1 >> "%TEMP%\StartupLog.txt" 2>&1
