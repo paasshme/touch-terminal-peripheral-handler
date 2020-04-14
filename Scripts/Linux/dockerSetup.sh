0cb8cf41dfd34a39330be188b5723333061f9704
Pashmiii <lowneonema@gmail.com>
2020-04-14 13:54:06 +0200

    Remove ssl errors from chromium in docker image

--- Scripts/Linux/dockerSetup.sh
+++ Scripts/Linux/dockerSetup.sh
@@ -1,2 +1,2 @@
 dotnet run --project /home/InteractiveTerminalCrossPlatformMicroservice/InteractiveTerminalCrossPlatformMicroservice.csproj &
-chromium-browser --kiosk https://localhost:5001/swagger/index.html --no-sandbox --noerrdialogs --disable-infobars --no-default-browser-check --no-experiments --no-pings --silent-debugger-extension-api 
+chromium-browser --kiosk https://localhost:5001/swagger/index.html --no-sandbox --noerrdialogs --disable-infobars --no-default-browser-check --no-experiments --no-pings --silent-debugger-extension-api --ignore-certificate-errors
